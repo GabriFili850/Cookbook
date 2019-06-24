d4c81a72ed6ef4a53c4dba3469750afc9fb3ae76
GabriFili850 <gabriele.filigheddu@hotmail.com>
2019-06-24 16:18:57 +0100

    app_id and app_key are been imported to App.js, and the form,  search bar and button created

--- src/App.js
+++ src/App.js
@@ -2,25 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const APP_ID = "e68ea2e3";
+  const APP_KEY = "e2b884a702c9d17c60ecb553245bcd23";
+
+  const exampleReq = 
+  "https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="serch-button" type="submit">Search</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;

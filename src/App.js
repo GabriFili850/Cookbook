a7ded3a147d69c0e99ee360166916105bb863c6a
GabriFili850 <gabriele.filigheddu@hotmail.com>
2019-06-24 20:49:37 +0100

    Recipe.js and App.js update

--- src/App.js
+++ src/App.js
@@ -8,14 +8,15 @@ const App = () => {
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
 
   useEffect( () => {
     getRecipes();
-  }, []);
+  }, [query]);
 
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=10&calories=591-722&health=alcohol-free`
       );
 
     const data = await response.json();
@@ -24,10 +25,21 @@ const App = () => {
     
   }
 
+  const updateSearch = e => {
+    setSearch(e.target.value);
+    console.log(search);
+    
+  }
+
+  const getSearch = e => {
+    e.preventDefault(); // to prevent refresh
+    setQuery(search);
+  }
+
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text" value={search}/>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
         <button className="serch-button" type="submit">
           Search
         </button>
@@ -35,9 +47,11 @@ const App = () => {
       {recipes.map(recipe => (
         //use .map because recepies is an array and i need to map all the objects, and i need to use a parenthesis because i want to return html/jsx
         <Recipe
+          key={recipe.recipe.label}
           title={recipe.recipe.label}
           calories={recipe.recipe.calories}
           image={recipe.recipe.image}
+          ingredients={recipe.recipe.ingredients}
         />
       ))}
     </div>

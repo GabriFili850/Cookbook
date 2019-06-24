d148cd0087b0ea8ae84d0b6dcacb9e849e8ca4d9
GabriFili850 <gabriele.filigheddu@hotmail.com>
2019-06-24 16:27:26 +0100

    counter creating but it is refreshing after every click on the button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
   const APP_ID = "e68ea2e3";
   const APP_KEY = "e2b884a702c9d17c60ecb553245bcd23";
-
   const exampleReq = 
   "https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free"
 
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log('Effect has been run');
+    
+  })
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text"/>
-        <button className="serch-button" type="submit">Search</button>
+        <button 
+          onClick={() => setCounter(counter + 1)}
+          className="serch-button"
+          type="submit"
+        >
+          {counter}
+        </button>
       </form>
     </div>
   );

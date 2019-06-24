1d5585c7e9b2a7678399bbda4438fdae053e48eb
GabriFili850 <gabriele.filigheddu@hotmail.com>
2019-06-24 16:55:56 +0100

    api is responding and it is logging out some values/data from the request(chickin in this case)

--- src/App.js
+++ src/App.js
@@ -4,25 +4,24 @@ import './App.css';
 const App = () => {
   const APP_ID = "e68ea2e3";
   const APP_KEY = "e2b884a702c9d17c60ecb553245bcd23";
-  const exampleReq = 
-  "https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free"
 
-  const [counter, setCounter] = useState(0);
 
-  useEffect(() => {
-    console.log('Effect has been run');
-    
-  })
+  useEffect( () => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`)
+    const data = await response.json();
+    console.log(data);  
+  }
+
   return (
     <div className="App">
       <form className="search-form">
-        <input className="search-bar" type="text"/>
-        <button 
-          onClick={() => setCounter(counter + 1)}
-          className="serch-button"
-          type="submit"
-        >
-          {counter}
+        <input className="search-bar" type="text" />
+        <button className="serch-button" type="submit">
+          Search
         </button>
       </form>
     </div>

f978ed4cd2051f5f8fccc8f745eb6f61c912a62b
GabriFili850 <gabriele.filigheddu@hotmail.com>
2019-06-25 13:45:00 +0100

    Bootstrap installed

--- src/App.js
+++ src/App.js
@@ -21,15 +21,15 @@ const App = () => {
       );
 
     const data = await response.json();
-    setRecipes(data.hits);  
+    setRecipes(data.hits);
     console.log(data.hits);
-    
+
   }
 
   const updateSearch = e => {
     setSearch(e.target.value);
     console.log(search);
-    
+
   }
 
   const getSearch = e => {
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -3,14 +3,16 @@ import React from 'react';
 const Recipe = ({title,calories,image,ingredients}) => {
     return (
         <div>
+          <div>
             <h1>{title}</h1>
             <ol>
                 {ingredients.map(ingredient => (
-                   <li>{ingredient.text}</li> 
+                   <li>{ingredient.text}</li>
                 ))}
             </ol>
             <p>{calories}</p>
             <img src={image} alt=""/>
+            </div>
         </div>
     );
 };
